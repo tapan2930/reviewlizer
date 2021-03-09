0e074de7ce6051a42c3146974016dd9616d0b681
Tapan Patel <tapan2930@gmail.com>
2021-03-09 09:33:58 +0530

    fixed erros 2

--- frontend/src/components/SkeletonLoader/index.tsx
+++ frontend/src/components/SkeletonLoader/index.tsx
@@ -13,7 +13,7 @@ const skeleton:React.FC<propType> = ({type,children}):React.ReactElement =>{
     )
 }
 
-const getTypeStyle(type:string)=>{
+const getTypeStyle = (type:string)=>{
     switch(type.toLowerCase()){
         case "text": return("h-4 bg-light-blue-400 rounded w-3/4");
         case "round": return("rounded-full bg-light-blue-400 h-12 w-12");
